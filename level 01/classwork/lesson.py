9174d009affbe8609e49b167611c247b5015d8f7
ananikotetradze-ops <ananikotetradze@gmail.com>
2026-02-05 22:53:56 +0400

    second commit

---
+++ level 01/classwork/lesson.py
@@ -0,0 +1,7 @@
+#variables
+name = "ana"
+age = 14
+last_name = "tetradze"
+print(name)
+print(age)  
+print(last_name)    
---
+++ level 01/homework/homework1.py
@@ -0,0 +1,51 @@
+#print gvadzlevs sashualebas informacia gamovitanot terminalshi
+print("My name is Ana Tetradze.")
+print("I am 14 years old.")    
+print("I live in Tbilisi, Georgia.")
+print("I study programming at GOA.")  
+print("I love coding and creating new projects.")
+
+#string aris brwyalebshi moqceuli monacemi
+print("This is a string.")
+print("Another example of a string.")
+print("i love satoru gojo")
+print("python is fun!") 
+print("nika is cool") 
+
+#integer aris mteli ricxvebi stringsa da mas shoris gansxvaveba aris rom erts brchxilebi wirdeba xolo meores ara
+print(10)
+print(25)  
+print(100)
+print(7)
+print(42)
+
+#float aris atwiladi ricxvebi
+print(10.5)
+print(3.14)
+print(7.25)
+print(0.99)
+print(15.75)
+
+#variables cvladebi gvexmareba davimaxsovrot mnishvnelovani informaciebi
+name = "Ana"   
+age = 14
+last_name = "Tetradze" 
+country = "Georgia"
+city = "Tbilisi"   
+print(name)
+print(age)
+print(last_name)
+print(country)
+print(city)
+
+#= aris miniwebis operatori cvladi + miniwebis operatori + mnishvneloba
+my_favorite_character = "Satoru Gojo"
+my_favorite_number = 6
+my_favorite_color = "Blue"
+my_favorite_food = "shaurma"
+my_favorite_animal = "seal"
+print(my_favorite_character)
+print(my_favorite_number)
+print(my_favorite_color)
+print(my_favorite_food)
+print(my_favorite_animal)
