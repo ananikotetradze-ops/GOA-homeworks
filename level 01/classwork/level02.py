dd66058d3943165b0db01d9a6c3641a24d675c46
ananikotetradze-ops <ananikotetradze@gmail.com>
2026-02-11 14:59:49 +0400

    third commit

---
+++ level 01/classwork/level02.py
@@ -0,0 +1,25 @@
+#gansxvaveba / da // shoris aris / yofs xolo // aris ramdenjer jdeba ert ricxvshi meore da tan rodesac or integers ertmanetze gavyoft pasuxi yoveltvis iqneba float magram // meshveobit chveuolebriv integeri gamodis
+number1 = 100
+number2 = 10
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 // number2)
+
+sum = 50 + 5
+substract = 50 - 5
+multiply = 50 * 5
+divide = 50 / 5
+print(sum)
+print(substract)    
+print(multiply)
+print(divide)
+
+age = 14
+age = age + 35
+age = age - 20
+age = age * 20
+
+print(age)
+
---
+++ level 01/homework/level02.py
@@ -0,0 +1,28 @@
+#matematikuri operatorebi pitonshi
+# mimateba romelic aginishneba '+'
+# gamokleba romeliic aaginishneba '-'
+# gamravleba romelic aaginishneba '*'
+# gayofa romelic aaginishneba '/'
+# xolo '//' aris tu ramdenjer motavsdeba erti ricxvi meoreshi
+# gansxvaveba / da // shoris aris '/' rodesac or integers gavyoft misi meshveobit pasuxi iqneba float magram '//' gvexmareba pasuxi integeri mivigot
+
+a = 94
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)   
+print(a // b)
+
+price = 49
+print(price - 12)
+
+score = 100
+score = 200
+print(score * 35)
+
+name = "barbare"
+name = "lika"
+name = "salome"
+print(name)
+#es shedegs gamoitans 'salome's radgan kompiuteri zemodan qvevit kitxulobs amitom dzvels daiviwyebs da axals gamoitans
