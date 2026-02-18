484d2dcdde2cf768e8247166350ffe49f584688c
ananikotetradze-ops <ananikotetradze@gmail.com>
2026-02-18 14:46:30 +0400

    fifth commit

---
+++ level 04/classwork/classwrk.py
@@ -0,0 +1,8 @@
+name = "ana"
+surname = "tetradze"
+country = "georgia"
+print('Hello, my name is ' + name + ' ' + surname + ' and I am from ' + country)
+
+#writing- codis dawera
+#executing- es nishnavs programis gashvebas anu roca cods vadzlevt brdzanebas da is 
+#debugging- erroris gasworeba
---
+++ level 04/homework/homework.py
@@ -0,0 +1,90 @@
+#konkatinacia- stringebis sheerteba
+
+#1
+name = "ana"
+surname = "tetradze"
+country = "georgia"
+favorite_subject = "qartuli"
+print('Hello, my name is ' + name + ' ' + surname + ' and I am from ' + country + '. My favorite subject is ' + favorite_subject)
+
+#2
+group = "group 94"
+print('I am a student of ' + group)
+
+#3
+#1. writing- codis dawera
+#2. executing- es nishnavs programis gashvebas
+#3. debugging- erroris gasworeba
+
+#4 
+# debbuging- nishnavs programashi shecdomebis povnas da gasworebas
+
+#5 
+# shecdomebis gasworeba. 
+
+#1academy = ‘GOA” araswori versia
+academi = 'GOA'  # swori versia
+
+#group$ = ’94 araswori versia
+group = '94'  # swori versia
+
+# prin(1academy) # araswori versia
+print(academi)  # swori versia
+
+#print(group$) # araswori versia
+print(group)  # swori versia
+
+
+# yvela matematikur operaciaze 5-5 magaliti
+
+# mimatebis operacia
+print(5 + 5)
+print('i am' + ' ' + 'ana')
+print('i am' + ' ' + 'ana' + ' ' + 'tetradze')
+print('i am' + ' ' + 'ana' + ' ' + 'tetradze' + ' ' + 'and i am from georgia')
+print('i am' + ' ' + 'ana' + ' ' + 'tetradze' + ' ' + 'and i am 14' + '. My favorite animal is seal')
+
+# gamoklebis operacia
+print(5 - 5)
+print(10 - 5)
+print(100 - 50)
+print(1000 - 500)
+print(96 - 2)
+
+# gamravlebis operacia
+print(5 * 5)
+print(10 * 5)
+print(100 * 50)
+print(1000 * 500)
+print(96 * 2)
+
+# gayofis operacia
+print(5 / 5)
+print(10 / 5)
+print(100 / 50)
+print(1000 / 500)
+print(96 / 2)
+
+# //- operacia
+print(5 // 5)
+print(10 // 5)
+print(100 // 50)
+print(1000 // 500)
+print(96 // 2)
+
+# %- operacia
+print(27 % 5)
+print(11 % 5)
+print(110 % 50)
+print(1050 % 500)
+print(97 % 2)
+
+# **- operacia
+print(5 ** 5)
+print(10 ** 5)
+print(100 ** 50)
+print(2 ** 5)
+print(96 ** 2)
+    
+
+
