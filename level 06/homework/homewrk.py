c9c1a5dcad5c83204b3ec4e9c1f5badb7230ed01
ananikotetradze-ops <ananikotetradze@gmail.com>
2026-02-25 13:49:02 +0400

    commit

---
+++ level 06/homework/homewrk.py
@@ -0,0 +1,21 @@
+#input-gvexmareba monacemi mivigot momxmareblisgan
+
+name = input("Enter your name: ")
+print(name)
+
+surname = input("Enter your surname: ")
+print(surname)
+
+age = input("Enter your age: ")
+print(age)
+
+#input- aris monacemi romelsac programa igebs
+#outpu-aris shedegi romelsac programa gvadzlevs
+
+#type()- gamoiyenebba imis gasagebat tu ra tipis monacemia konkretuli cvladi
+print(type(name))
+print(type(surname))
+print(type(age))
+
+# boloshi gvwirdeba py radgan compiuterma gaigos ra tipis failia da programa gaxsnas
+
