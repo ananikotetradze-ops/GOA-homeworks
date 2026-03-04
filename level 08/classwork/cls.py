12b3f2a66d5a14af740016a9434eb0117881f05c
ananikotetradze-ops <ananikotetradze@gmail.com>
2026-03-04 16:27:27 +0400

    commit

---
+++ level 08/classwork/cls.py
@@ -0,0 +1,11 @@
+# int()- misi meshveobit monacemis integerad gadaqceva shegvidzlia
+# str()- misi meshveobit monacemis stringad gadaqceva shegvidzlia
+# float()- misi meshveobit monacemis floatad gadaqceva shegvidzlia
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("mimateba", num1 + num2)
+print("gamokleba", num1 - num2)
+print("gamravleba", num1 * num2)
+print("gayofa", num1 / num2)
---
+++ level 08/homework/hmwrk.py
@@ -0,0 +1,7 @@
+name = input("enter your name:")
+print("hello", name)
+
+# 3
+number = int(input("enter a number:"))
+word = input("enter a word:")
+print(number * word)
