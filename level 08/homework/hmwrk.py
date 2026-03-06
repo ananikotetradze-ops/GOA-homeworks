4c772727612218e471734e4123de114457731d95
ananikotetradze-ops <ananikotetradze@gmail.com>
2026-03-06 16:02:51 +0400

    commit

--- level 08/homework/hmwrk.py
+++ level 08/homework/hmwrk.py
@@ -1,7 +1,24 @@
 name = input("enter your name:")
 print("hello", name)
 
-# 3
+#input- misi meshveobit shegvidzlia momxmarebels shemovataninot raime monacemi
+#output-shedegs romelsac programa gvadzlevs
+#type()-misi meshveobit monacemis tipis gageba shegvidzlia
+#int()- misi meshveobit monacemis integerad gadaqceva shegvidzlia
+#str()- misi meshveobit monacemis stringad gadaqceva shegvidzlia
+#float()- misi meshveobit monacemis floatad gadaqceva shegvidzlia   
+
+
+#3
 number = int(input("enter a number:"))
 word = input("enter a word:")
 print(number * word)
+
+
+#4
+name = input("enter your name:")
+surname = input("enter your surname:")
+age = int(input("enter your age:"))
+print("your name is", name)
+print("your surname is", surname)
+print("your age is", age)
