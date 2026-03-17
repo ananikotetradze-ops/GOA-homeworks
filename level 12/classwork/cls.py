c83ac69553276e497f4a3c9276b32bad932cd536
ananikotetradze-ops <ananikotetradze@gmail.com>
2026-03-17 22:31:59 +0400

    commit

---
+++ level 12/classwork/cls.py
@@ -0,0 +1,11 @@
+# or is shemtxvevashi true gamova mashin rodesac erterti mainc iqneba true an orive
+#magalitad print (true or true) an print(false or true)
+#and is shemtxvevashi true gamova mashin rodesac orive davaleba iqneba shesrulebuli
+#print(True and True)
+#<- nakleboba
+#>- metoba
+#==- udris
+#!=- ar udris
+#>=- metia an tolia
+#<=- naklebia an tolia
+print(False or True)
---
+++ level 12/homework/hm.py
@@ -0,0 +1,15 @@
+#shedarebis operatorebi
+#<- nakleboba
+#>- metoba
+#==- udris
+#!=- ar udris
+#>=- metia an tolia
+#<=- naklebia an tolia
+
+#logikuri operatorebi
+# or- an
+# and- da
+
+#boolean-true/false 
+
+print(True and not False) or (False and True) or (not (False or False) and True) and (True or not (False and True))
