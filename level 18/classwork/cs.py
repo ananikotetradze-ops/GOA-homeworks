afff83d31b0871fd25cd04ee013937bacba39f80
ananikotetradze-ops <ananikotetradze@gmail.com>
2026-04-08 13:40:07 +0400

    commit

---
+++ level 18/classwork/cs.py
@@ -0,0 +1,24 @@
+#for loop ebs gamoviyenebt mashin roca gvinda kodi ramdenjerme gavameorot
+for i in range(10):
+    print(i)
+#for-keyword romelic qmnis for loop-s
+#i-cvladi romelic yovel iteraciaze anu gameorebaze axal mnishvnelobas igebs
+#in- anu "shi" - sad unda eweros mnishvneloba
+#range()- es aris  funqcia romelic qmnis ricxvebis mimdevrobas
+#print(i)- eubneba pitons ro dabewdos cvladi i
+#konkretulad es magaliti gamoitans:0,1,2,3,4,5,6,7,8,9
+
+
+    # sum = 0
+    # for i in range(10):
+    #     num = int(input("Enter a number: "))
+    #     sum = sum + num
+    # print("jami aris:", sum)
+
+password = "lucky123"
+under_pass = "enter your password: "
+while under_pass != password:
+    under_pass = input("enter your password: ")
+    print("wrong password, try again")
+    under_pass = input("enter your password: ")
+    print("welcome")
---
+++ level 18/homework/hm.py
@@ -0,0 +1,17 @@
+#for loop-vicit gansazgvruli raodenoba da vutitebt chvenit
+#while loop- zustad ar vicit gansazgvruli raodenoba da mushaobs manamde sanam piroba weshmeariti ar iqneba
+
+
+
+password = "blabla123"
+under_pass = input("enter your password: ")
+while under_pass != password:
+    print("wrong password, try again")
+    under_pass = input("enter your password: ")
+    print("welcome")
+
+    i= 1
+    while i <= 10:
+        print(i)
+        i = i + 1
+        
